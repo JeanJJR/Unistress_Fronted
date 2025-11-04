7eba617ddd16c445f109d7a47634cb8fd5fc8084
N1c01e03 <153241553+N1c01e03@users.noreply.github.com>
2025-11-04 01:59:06 -0500

    Vista emocional html y css

--- src/app/componente/vista-emocional-component/vista-emocional-component.ts
+++ src/app/componente/vista-emocional-component/vista-emocional-component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCard} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-vista-emocional-component',
-  imports: [],
+  imports: [
+    MatFormFieldModule, MatInputModule, MatButtonModule, MatCard,
+  ],
   templateUrl: './vista-emocional-component.html',
   styleUrl: './vista-emocional-component.css',
 })
